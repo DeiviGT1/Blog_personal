cd6bfcb0a5cd8a80eb6b187192630431660b6023
David <josedago1163@gmail.com>
2022-11-21 23:08:19 -0500

    añadimos al js la funcion para cambiar el modo

--- JS_files/index.js
+++ JS_files/index.js
@@ -1,27 +1,48 @@
-
 // Funcion para trasladar en el eje Y
 
-var imagen = document.getElementById('imagen');
+var imagen = document.getElementById("imagen");
 
-    (function(){
-        var regulador = function(type,name){
-            var running = false;    
-            var func = function(){
-                if (running){ return;}
-                running = true;
-                requestAnimationFrame(function(){
-                    window.dispatchEvent(new CustomEvent(name))
-                    running=false
-                });
-            };
-            window.addEventListener(type, func)
-        };
-        regulador("scroll", "optimizedScroll")
-    })();
+(function () {
+  var regulador = function (type, name) {
+    var running = false;
+    var func = function () {
+      if (running) {
+        return;
+      }
+      running = true;
+      requestAnimationFrame(function () {
+        window.dispatchEvent(new CustomEvent(name));
+        running = false;
+      });
+    };
+    window.addEventListener(type, func);
+  };
+  regulador("scroll", "optimizedScroll");
+})();
 
-window.addEventListener("optimizedScroll", function(){
-    imagen.style.transform = "translateY(-" + window.pageYOffset + "px)";
-})
+window.addEventListener("optimizedScroll", function () {
+  imagen.style.transform = "translateY(-" + window.pageYOffset + "px)";
+});
 
 // Funcion para color fondo entero
 
+function modo() {
+  var modo_actual = document.getElementById("modo_actual_pagina");
+  if (modo_actual.src.substr(modo_actual.src.length - 8) == "luna.png") {
+    
+    var nueva_pagina = document
+      .getElementById("modo_actual_pagina")
+      .src.substr(
+        -document.getElementById("modo_actual_pagina").src.length,
+        document.getElementById("modo_actual_pagina").src.length - 8
+      ) + "sol.png";
+  } else {
+    var nueva_pagina = document
+      .getElementById("modo_actual_pagina")
+      .src.substr(
+        -document.getElementById("modo_actual_pagina").src.length,
+        document.getElementById("modo_actual_pagina").src.length - 8
+      ) + "luna.png";
+  }
+  return nueva_pagina;
+}
