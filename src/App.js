ed971f6d833fb72af870e873cb098260c70c30f0
David <josedago1163@gmail.com>
2022-12-25 18:10:44 -0500

    intentamos con muchas rutas pero no funciona

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import { Router } from '@reach/router';
+import {Routes, Route, BrowserRouter, useLocation} from "react-router-dom"
 import { Home } from './Home';
 
 function App() {
-  console.log("contador")
+  
   return (
-    <Router>
-      <Home path= '/'/>
-      <Home path= '/contador'/>
-    </Router>
+    <BrowserRouter>
+      <MyComponent/>
+      <Routes>
+        <Route exact path= "/" element={<Home />}/>
+        <Route exact path="/" element ={<Home />}/>
+        <Route exact path="/contador.html" element ={<Home />}/>
+        <Route exact path="/contador" element ={<Home />}/>
+        <Route exact path="contador" element ={<Home />}/>
+        <Route exact path="contador.html" element ={<Home />}/>
+        <Route exact path="/public/contador.html" element ={<Home />}/>
+        <Route exact path="/public/contador" element ={<Home />}/>
+        <Route exact path="../public/contador.html" element ={<Home />}/>
+        <Route exact path="../public/contador" element ={<Home />}/>
+        <Route exact path="http://localhost:3000/contador.html" element ={<Home />}/>
+        <Route exact path="http://localhost:3000/contador" element ={<Home />}/>
+        <Route exact path="./contador.html" element ={<Home />}/>
+        <Route exact path="./contador" element ={<Home />}/>
+      </Routes>
+    </BrowserRouter>
+    
   );
 }
 
-export default App;
+function MyComponent() {
+  const location = useLocation()
+  const currentPath = location.pathname
+  console.log(currentPath)
+}
+
+export default App
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,9 @@
 function Home() { 
     console.log("Home")
+
+    return (
+        <p>HOLA</p>
+    )
 }
 
 export {Home}
